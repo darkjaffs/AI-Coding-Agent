c0f27d35a9f8b18b5b266aee89dd13028501503d
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-10-06 00:37:30 +0500

    Add functionality to run a file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os 
-from dotenv import load_dotenv # type: ignore
+import os
+from dotenv import load_dotenv  # type: ignore
 from google import genai
 import sys
-from google.genai import types # type: ignore
+from google.genai import types  # type: ignore
 
 load_dotenv()
 
@@ -10,16 +10,14 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 user_prompt = sys.argv[1]
 messages = [
-    types.Content(role="user", 
-                  parts=[types.Part(text=user_prompt)]),
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
 ]
 
 client = genai.Client(api_key=api_key)
 
 try:
     response = client.models.generate_content(
-        model = "gemini-2.0-flash-001",
-        contents = sys.argv[1]
+        model="gemini-2.0-flash-001", contents=sys.argv[1]
     )
 except:
     print("No Input Detected")

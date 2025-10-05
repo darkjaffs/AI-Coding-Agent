94470f48b234dcb40b5276d27d46a8e37df0176a
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-10-05 17:27:22 +0500

    Initial Model Done and User Input Setup Complete

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from ai-coding-agent!")
+import os 
+from dotenv import load_dotenv
+from google import genai
+import sys
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+try:
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash-001",
+        contents = sys.argv[1]
+    )
+except:
+    print("No Input Detected")
+    raise Exception("Code 1")
+
+print(response.text)
+
+print("Prompt Tokens: ", response.usage_metadata.prompt_token_count)
+print("Response Tokens: ", response.usage_metadata.candidates_token_count)

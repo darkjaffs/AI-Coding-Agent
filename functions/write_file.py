beab39018ca764f84bdb783f78eb3ff67349b566
darkjaffs <121581291+darkjaffs@users.noreply.github.com>
2025-10-05 21:45:05 +0500

    Create Functions to Read and Write Files

---
+++ functions/write_file.py
@@ -0,0 +1,27 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    
+    absdirectory = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    
+    if not abs_file_path.startswith(absdirectory):
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    if not os.path.exists(abs_file_path):
+        try:
+            os.makedirs(os.path.dirname(abs_file_path), exist_ok=True)
+        except Exception as e:
+            return f'Error Creating Directory: {e}'
+        
+    if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
+        return f'Error: "{file_path}" is a directory, not a file'
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+        return (
+            f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+        )
+    except Exception as e:
+        return f'Error: Writing into file: {e}'
+    
